4981fcd0f2bd52f6ea2827f1987f4898ce9f27f7
hsx <hanxiang@sightp.com>
2024-02-18 14:05:14 +0800

    sync: 图片特征点持久化存储 & 使用存储特征点与目标图片匹配

--- matcher/core/match.py
+++ matcher/core/match.py
@@ -1,6 +1,5 @@
 import cv2
-import os
-import h5py
+from storage import ImageDataSet
 
 # 读取目标图片
 target_image_path = "matcher/home.jpg"
@@ -10,53 +9,29 @@ target_image = cv2.imread(target_image_path, cv2.IMREAD_GRAYSCALE)
 sift = cv2.SIFT_create(nfeatures=500, edgeThreshold=10, sigma=1.6)
 # 提取目标图片的特征点和描述子
 kp1, des1 = sift.detectAndCompute(target_image, None)
-# # 读取 HDF5 文件
-# with h5py.File('matcher/features.h5', 'r') as f:
-#     # 读取关键点数据
-#     kp_data = f['keypoints'][:]
-#     kp1 = []
-
-#     # 解析关键点数据
-#     for pt, size, angle, response, octave, class_id in kp_data:
-#         kp = cv2.KeyPoint(x=pt[0], y=pt[1], size=size, angle=angle, response=response, octave=octave, class_id=class_id)
-#         kp1.append(kp)
-
-#     # 读取描述子数据
-#     des1 = f['descriptors'][:]
-
-print("Keypoints shape:", len(kp1))
 # 设置一个阈值，用于筛选匹配对
 threshold = 0.75
 
-# 遍历文件夹中的图片文件
-image_folder = 'matcher/pics'
+dataset = ImageDataSet("foo")
 best_match_image_path = None
 best_match_similarity = 0
-output_image = None
-for filename in os.listdir(image_folder):
-    if filename.endswith('.jpg') or filename.endswith('.png'):
-        # 读取文件
-        image_path = os.path.join(image_folder, filename)
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+# output_image = None
 
-        # 提取当前图片的特征点和描述子
-        kp2, des2 = sift.detectAndCompute(image, None)
+for image_name, (keypoints, descriptors) in dataset.read_all():
+    print(image_name, len(keypoints), len(descriptors))
+    # 使用BFMatcher进行特征匹配
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, descriptors, k=2)
 
-        # 使用BFMatcher进行特征匹配
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des1, des2, k=2)
+    # # 计算相似度比例
+    good_matches = [m for m, n in matches if m.distance < threshold * n.distance]
+    similarity = len(good_matches) / len(matches)
 
-        # 计算相似度比例
-        good_matches = [m for m, n in matches if m.distance < threshold * n.distance]
-        similarity = len(good_matches) / len(matches)
+    # 更新最佳匹配
+    if similarity > best_match_similarity:
+        best_match_similarity = similarity
+        best_match_image_path = image_name
+    # output_image = cv2.drawMatches(target_image, kp1, image, kp2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-        # 更新最佳匹配
-        if similarity > best_match_similarity:
-            best_match_similarity = similarity
-            best_match_image_path = image_path
-            output_image = cv2.drawMatches(target_image, kp1, image, kp2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-cv2.imwrite("matcher/result.jpg", output_image)
-
-# 返回相似度最高的图片路径
+# # 返回相似度最高的图片路径
 print(f'Best match image path: {best_match_image_path}, Similarity: {best_match_similarity}')
--- matcher/core/storage.py
+++ matcher/core/storage.py
@@ -2,68 +2,73 @@ import h5py
 import numpy as np
 import cv2
 import os
-
-# 读取 HDF5 文件
-with h5py.File('matcher/features.h5', 'r') as f:
-    # 读取关键点数据
-    kp_data = f['keypoints'][:]
-    keypoints = []
-
-    # 解析关键点数据
-    for pt, size, angle, response, octave, class_id in kp_data:
-        kp = cv2.KeyPoint(x=pt[0], y=pt[1], size=size, angle=angle, response=response, octave=octave, class_id=class_id)
-        keypoints.append(kp)
-
-    # 读取描述子数据
-    des = f['descriptors'][:]
-
-# 打印关键点数据和描述子数据的形状
-print("Keypoints shape:", len(keypoints))
-print("Descriptors shape:", des.shape)
-
-# 打印前几个关键点和描述子的值
-print("First few keypoints:", keypoints[:5])
-print("First few descriptors:", des[:5])
+from tqdm import tqdm
 
 class ImageDataSet:
-    def __init__(self, folder_path, dataset_name):
+    def __init__(self, fetch_folder_name, force_update=False):
+        if not os.path.exists(fetch_folder_name):
+            print(f"Folder {fetch_folder_name} not found.")
+            return
+
         _path = os.path.dirname(os.path.abspath(__file__))
-        self.folder_path = os.path.join(_path, "../..", folder_path)
+        self.fetch_folder_path = os.path.join(_path, "../..", fetch_folder_name)
+        self.save_folder_path = os.path.join(_path, "../..", "matcher")
 
-        self.dataset_name = dataset_name
+        self.dataset_name = fetch_folder_name
         self.sift = cv2.SIFT_create()
+        if (not os.path.exists(f'{self.save_folder_path}/{self.dataset_name}.h5')) or force_update:
+            self.__create_dataset__()
+            self.save_all()
+        else:
+            self.__create_dataset__()
 
+    def __create_dataset__(self):
+        self.waiting_list = []
+        for filename in os.listdir(self.fetch_folder_path):
+            if filename.endswith('.jpg') or filename.endswith('.png'):
+                image_path = os.path.join(self.fetch_folder_path, filename)
+                # print(image_path, filename, self.dataset_name)
+                self.waiting_list.append((image_path, filename.split('.')[0]))
 
-    def create_dataset(self):
-        with h5py.File(self.dataset_name, 'w') as f:
-            for filename in os.listdir(self.folder_path):
-                if filename.endswith('.jpg') or filename.endswith('.png'):
-                    image_path = os.path.join(self.folder_path, filename)
-                    self.fetch_image(image_path)
-
-    def read_image(self, image_name):
-        with h5py.File(self.dataset_name, 'r') as f:
+    def read(self, image_name):
+        """ Read keypoints and descriptors from the dataset with image_name."""
+        with h5py.File(f'{self.save_folder_path}/{self.dataset_name}.h5', 'r') as f:
             if image_name in f:
-                image_data = f[image_name][:]
-                return image_data
+                image_data = f[image_name]
+                keypoints = []
+                for pt, size, angle, response, octave, class_id in image_data['keypoints'][:]:
+                    kp = cv2.KeyPoint(x=pt[0], y=pt[1], size=size, angle=angle, response=response, octave=octave, class_id=class_id)
+                    keypoints.append(kp)
+                print(len(keypoints), len(image_data['descriptors'][:]))
+                return (image_name, (keypoints, image_data['descriptors'][:]))
             else:
                 print(f"Image {image_name} not found in the dataset.")
+                return (image_name, (None, None))
 
-    def fetch_image(self, image_path):
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    def read_all(self):
+        """ Read all keypoints and descriptors from the dataset. """
+        return [self.read(image_name) for _, image_name in self.waiting_list]
 
-        kp, des = self.sift.detectAndCompute(image, None)
+    def save_all(self):
+        """ Extract features from images and save to HDF5 file. """
+        if len(self.waiting_list) == 0:
+            print("No image to fetch.")
+            return
 
-        with h5py.File('matcher/features.h5', 'w') as f:
-            group = f.create_group(image_path)
+        with h5py.File(f'{self.save_folder_path}/{self.dataset_name}.h5', 'w') as f:
+                for image_path, filename in tqdm(self.waiting_list, desc=f"Extracting features from {self.dataset_name}...", total=len(self.waiting_list)):
+                    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-            kp_data = np.array([(kp[i].pt, kp[i].size, kp[i].angle, kp[i].response, kp[i].octave, kp[i].class_id) for i in range(len(kp))], dtype=[('pt', 'f4', 2), ('size', 'f4'), ('angle', 'f4'), ('response', 'f4'), ('octave', 'i4'), ('class_id', 'i4')])
-            group.create_dataset('keypoints', data=kp_data)
-            group.create_dataset('descriptors', data=des)
+                    kp, des = self.sift.detectAndCompute(image, None)
+                    group = f.create_group(filename)
+                    print(f"Group {filename} created.")
 
-# TODO: 1. Create a new instance of the ImageDataSet class
-#       2. Call the create_dataset method on the instance
-#       3. Call the read_image method on the instance
-#       4. Print the returned image data
-#       5. Call the fetch_image method on the instance
-        
+                    kp_data = np.array([(kp[i].pt, kp[i].size, kp[i].angle, kp[i].response, kp[i].octave, kp[i].class_id) for i in range(len(kp))], dtype=[('pt', 'f4', 2), ('size', 'f4'), ('angle', 'f4'), ('response', 'f4'), ('octave', 'i4'), ('class_id', 'i4')])
+                    group.create_dataset('keypoints', data=kp_data)
+                    group.create_dataset('descriptors', data=des)
+    
+    def append_one(self, image_name):
+        """ Append one image to the dataset. """
+        if not os.path.exists(image_name):
+            print(f"Image {image_name} not found.")
+            return

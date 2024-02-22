fc15147e1ae00faddf0a3976ae842585ac9a7757
hsx <hanxiang@sightp.com>
2024-02-23 00:50:29 +0800

    feat: 添加性能调试包

--- fast_match.py
+++ fast_match.py
@@ -1,3 +1,13 @@
 from matcher.core.match import Matcher
-matcher = Matcher("foo", debug=True)
-matcher.match("static/foo/bottom.jpg")
+from line_profiler import LineProfiler
+
+def main():
+    matcher = Matcher("foo", debug=True)
+    matcher.match("static/foo/bottom.jpg")
+
+if __name__ == "__main__":
+    profiler = LineProfiler()
+    profiler_wrapper = profiler(main)
+    profiler_wrapper()
+    profiler.print_stats()
+    profiler.dump_stats("fast_match.lprof")
